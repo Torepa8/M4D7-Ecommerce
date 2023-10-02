86b9fe2e3a70de0d16ee95c51cfbbf9ae560cc62
Torepa8 <65725642+Torepa8@users.noreply.github.com>
2023-10-02 21:43:35 +0200

    add file

---
+++ script.js
@@ -0,0 +1,5 @@
+fetch("https://striveschool-api.herokuapp.com/api/put-your-endpoint-here/", {
+headers: {
+    "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTFiMWI3YzM5MzI3YzAwMThkM2EyYjYiLCJpYXQiOjE2OTYyNzUzMjUsImV4cCI6MTY5NzQ4NDkyNX0.vww_zKkpOGlO7u-I13sFxDxHeNkvp-lo54e_5w5ag84"
+    }
+    })
